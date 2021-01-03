3ead18065f0eec6396e36c29b08beb9e62cbb19c
claudinemahoro <claudinemah3@gmail.com>
2021-01-03 22:15:26 +0200

    creating the welcome view

--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+# from .models import Image,User,Profile,Follow,Comment
+# from .forms import ImageForm,UpdateProfile,CommentForm
 # Create your views here.
+
+@login_required(login_url='/accounts/login/')
+def welcome(request):
+    current_user= request.user
+    all_images=Image.objects.all()
+    profile=Profile.objects.all()
+    return render(request, 'all-views/index.html',{"images":all_images})
