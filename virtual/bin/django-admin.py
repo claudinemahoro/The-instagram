e0b2bcc53fad6488259d9885c45cfb58a196afc2
claudinemahoro <claudinemah3@gmail.com>
2021-01-03 20:20:50 +0200

    starting the project

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/claudine/Documents/Wecode-Projects/the-instagram/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
